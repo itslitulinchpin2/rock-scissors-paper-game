4137d490c1094f612bdd6d912e6c5ebee45e43bf
itslitulinchpin2 <itslitulinchpin@gmail.com>
2023-10-09 04:07:29 +0200

    initial description for project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+
+//1. 박스 2개(타이틀, 사진, 경기결과값)
+//2. 가위 바위 보 버튼, 
+//3. 클릭시 게임이 진행된다. 클릭한 값이 박스에 보임.
+//4. 컴퓨터는 랜덤하게 아이템 선택이 된다.
+//5. 3,4의 결과를 가지고 승패를 따진다.
+//6. 승패 결과에 따라 박스 테두리 색상을 변경한다.(승-초록 패-빨강 비김-검정색)
 
 function App() {
+
+
+  
   return (
     <div>
-      Rock Scissors Paper!
+     
     </div>
   );
 }
